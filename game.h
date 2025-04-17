21d3ccdcddb546f12e435a33bcba79909f553480
celine-hui <ccelinehui@gmail.com>
2025-04-17 15:44:44 -0400

    Created foundation for game class

--- game.h
+++ game.h
@@ -1,12 +1,23 @@
-//
-// Created by cesar on 4/17/2025.
-//
-
 #ifndef DSAPROJECT3_GAME_H
 #define DSAPROJECT3_GAME_H
-
+#include <unordered_map>
+#include <vector>
+using namespace std;
 
 class game {
+    private:
+        unordered_map<string, vector<string>> genreTable; //key: genre, value: list of games
+        unordered_map<string, vector<string>> maturityTable; //key: maturity rating, value: list of games
+        unordered_map<string, vector<string>> consoleTable; //key: console, value: list of games
+        unordered_map<string, vector<string>> multiplayerTable; //key: single/multi, value: list of games
+        unordered_map<string, vector<string>> scoreTable; //key: score range, value: list of games
+    public:
+        game(string filePath);
+        vector<string> getGenre(string genre); //input is key for genreTable
+        vector<string> getMaturity(string maturity);
+        vector<string> getConsole(string console);
+        vector<string> getMultiplayer(string multiplayer);
+        vector<string> getScore(string score);
 
 };
 
--- gameTable.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by celin on 4/17/2025.
-//
-
-#include "gameTable.h"
